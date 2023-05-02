abc500884fb0d27844a8ca8f5186e7cdd4717e95
GhostDoseAdmin <ghostdosebros@gmail.com>
2023-05-01 23:13:01 -0600

    gg
    gg

--- Game/Assets/__GHOST DOSE/Scripts/System/VictimControl.cs
+++ Game/Assets/__GHOST DOSE/Scripts/System/VictimControl.cs
@@ -14,13 +14,14 @@ public class VictimControl : Item
     public GameObject ChosenVictim;
     public GameObject trigger;
     private bool startCircle;
-
+    public GameObject main;
+    Vector3 mainStartPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.instance.Play("EnemyEngage", null);
         ChooseVictim();
+        mainStartPos = transform.position;
     }
 
     // Update is called once per frame
@@ -28,7 +29,13 @@ public class VictimControl : Item
     {
         if(startCircle)
         {
-            transform.Rotate(0f, 5f * Time.deltaTime, 0f);
+            main.transform.Rotate(0f, 5f * Time.deltaTime, 0f);
+            if(main.transform.position.y < mainStartPos.y+1)
+            {
+                Vector3 currPos = main.transform.position;
+                currPos.y += 0.02f;
+                main.transform.position = currPos;//descend
+            }
             //KEEP IN CIRCLE
             if (Vector3.Distance(GameDriver.instance.Player.transform.position, transform.position) > 3) { GameDriver.instance.Player.transform.position = Vector3.Lerp(GameDriver.instance.Player.transform.position, transform.position, 0.02f); }
             if (GameDriver.instance.twoPlayer) { if (Vector3.Distance(GameDriver.instance.Client.transform.position, transform.position) > 3) { GameDriver.instance.Client.transform.position = Vector3.Lerp(GameDriver.instance.Client.transform.position, transform.position, 0.02f); } }
@@ -47,6 +54,7 @@ public class VictimControl : Item
         if ((playerOn && clientOn && GameDriver.instance.twoPlayer) || (!GameDriver.instance.twoPlayer && playerOn))
         {
             startCircle = true;
+            AudioManager.instance.Play("demoncircle", null);
             GameDriver.instance.WriteGuiMsg("Beware to summon ZOZO", 5f);
             trigger.SetActive(false);
         }
