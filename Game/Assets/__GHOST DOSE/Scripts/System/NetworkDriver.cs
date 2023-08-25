1721e20ee87033a45e32db8bf845589c75f049e4
GhostDoseAdmin <ghostdosebros@gmail.com>
2023-08-25 17:21:13 -0600

    gg
    gg

--- Game/Assets/__GHOST DOSE/Scripts/System/NetworkDriver.cs
+++ Game/Assets/__GHOST DOSE/Scripts/System/NetworkDriver.cs
@@ -423,6 +423,7 @@ namespace NetworkSystem
                                 if (dict["type"] == "med") { obj.GetComponent<FirstAidKit>().DestroyWithSound(true); }
                                 if (dict["type"] == "bat") { obj.GetComponent<Battery>().DestroyWithSound(true); }
                                 if (dict["type"] == "grid") { obj.GetComponent<laserGridItem>().DestroyWithSound(true); }
+                                if (dict["type"] == "rem") { obj.GetComponent<remPodItem>().DestroyWithSound(true); }
                                 if (dict["type"] == "cand") { obj.GetComponent<Candle>().DestroyWithSound(true); LevelManager.GetComponentInChildren<VictimControl>().candleCount++; }
                             }
                             else { if (dict["type"] == "key") { KeyInventory.instance.RemoveKey(dict["pass"]); } }//local player already picked up
