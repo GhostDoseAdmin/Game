70a54149324f506f898e88a61b02bafaa4d62ca6
GhostDoseAdmin <ghostdosebros@gmail.com>
2023-09-29 18:45:14 -0600

    gg
    gg

--- Game/Assets/__GHOST DOSE/Scripts/System/VictimControl.cs
+++ Game/Assets/__GHOST DOSE/Scripts/System/VictimControl.cs
@@ -493,7 +493,7 @@ public class VictimControl : Item
 
     public void SpawnZOZO()
     {
-        ZOZO.GetComponent<ZozoControl>().HP = 1000;
+        ZOZO.GetComponent<ZozoControl>().HP = ZOZO.GetComponent<ZozoControl>().HP = ZOZO.GetComponent<ZozoControl>().MAXHP;
         ZOZO.GetComponent<ZozoControl>().DEAD = false;
         ZOZO.SetActive(true);
        
